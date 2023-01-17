2f431be1bd482b41f68735c9f3cf95514952d4a3
ryo2808 <122766667+ryo2808@users.noreply.github.com>
2023-01-17 13:38:33 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,79 @@
 'use strict';
 
 
+
+{
+  const prev = document.getElementById('prev');
+  const next = document.getElementById('next');
+  const ul = document.querySelector('.pic-container ul');
+  const slides = ul.children;
+  let currentIndex = 0;
+  const dots = [];
+
+  function updateButtons() {
+
+    prev.classList.remove('hidden');
+    next.classList.remove('hidden');
+
+    if (currentIndex === 0) {
+      prev.classList.add('hidden');
+    }
+    if (currentIndex === slides.length - 1) {
+      next.classList.add('hidden');
+    }
+  }
+
+  function moveSlides() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-100 * currentIndex}%)`;
+  }
+
+  function setupDots() {
+    for (let i = 0; i < slides.length; i++) {
+      const button = document.createElement('button');
+      button.addEventListener('click', () => {
+        currentIndex = i
+        updateDots();
+        updateButtons();
+        moveSlides();
+      });
+      dots.push(button);
+      document.querySelector('.carousel nav').appendChild(button);
+    }
+    dots[0].classList.add('current');
+  }
+
+  function updateDots() {
+    dots.forEach(dot => {
+      dot.classList.remove('current');
+    });
+    dots[currentIndex].classList.add('current');
+  }
+
+  updateButtons();
+  setupDots();
+  moveSlides();
+
+  next.addEventListener('click', () => {
+    currentIndex++;
+    updateButtons();
+    updateDots();
+    moveSlides();
+  });
+  prev.addEventListener('click', () => {
+    currentIndex--;
+    updateButtons();
+    updateDots();
+    moveSlides();
+  });
+
+  window.addEventListener('resize', () => {
+    moveSlides();
+  });
+}
+
+
+
 {
   function inviewCallback(entries, obs) {
     entries.forEach(entry => {
